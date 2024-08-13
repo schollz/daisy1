fa18ae7264417fd746f56413dc689dc234e3371a
Zack Scholl <zack.scholl@gmail.com>
2024-08-13 07:20:45 -0700

    not working

--- main.cpp
+++ main.cpp
@@ -15,16 +15,6 @@ FatFSInterface fsi;
 FIL SDFile;
 
 int main(void) {
-  hw.Configure();
-  hw.Init();
-  /* true == wait for PC: will block until a terminal is connected */
-  hw.StartLog(true);
-
-  /** check that floating point printf is supported
-   * linker flags modified in the Makefile to enable this
-   */
-  hw.PrintLine("Verify CRT floating point format: %.3f", 124.0f);
-
   /* test sd card */
   // Vars and buffs.
   char outbuff[512];
@@ -36,10 +26,21 @@ int main(void) {
   len = strlen(outbuff);
   failcnt = 0;
 
+  hw.Configure();
+  hw.Init();
+  /* true == wait for PC: will block until a terminal is connected */
+  hw.StartLog(true);
+
+  /** check that floating point printf is supported
+   * linker flags modified in the Makefile to enable this
+   */
+  hw.PrintLine("Verify CRT floating point format: %.3f", 124.0f);
+
   hw.PrintLine("Starting SD Card Test...");
   // Init SD Card
   SdmmcHandler::Config sd_cfg;
   sd_cfg.Defaults();
+  sd_cfg.speed = SdmmcHandler::Speed::SLOW;
   sd.Init(sd_cfg);
 
   hw.PrintLine("SD Card Initialized!");
@@ -48,19 +49,33 @@ int main(void) {
 
   hw.PrintLine("Mounting SD Card...");
   // Mount SD Card
-  f_mount(&fsi.GetSDFileSystem(), "/", 1);
-  hw.PrintLine("SD Card Mounted!");
+  if (f_mount(&fsi.GetSDFileSystem(), "/", 1) == FR_OK) {
+    hw.PrintLine("SD Card Mounted!");
+  } else {
+    System::Delay(500);
+    if (f_mount(&fsi.GetSDFileSystem(), "/", 1) == FR_OK) {
+      hw.PrintLine("SD Card Mounted!");
+    } else {
+      hw.PrintLine("Error mounting SD Card!");
+    }
+  }
+
   // Open and write the test file to the SD Card.
   if (f_open(&SDFile, TEST_FILE_NAME, (FA_CREATE_ALWAYS) | (FA_WRITE)) ==
       FR_OK) {
     f_write(&SDFile, outbuff, len, &byteswritten);
+    hw.PrintLine("Wrote %d bytes to file.", byteswritten);
     f_close(&SDFile);
+  } else {
+    hw.PrintLine("Error opening file to write!");
   }
 
   // Read back the test file from the SD Card.
   if (f_open(&SDFile, TEST_FILE_NAME, FA_READ) == FR_OK) {
     f_read(&SDFile, inbuff, len, &byteswritten);
     f_close(&SDFile);
+  } else {
+    hw.PrintLine("Error opening file to read!");
   }
 
   // Check for sameness.

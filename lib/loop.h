61047e6f63f63348f3d05b15cd7566f0c691fb1f
Zack <zack.scholl@gmail.com>
2024-08-13 17:17:45 -0700

    more

--- lib/loop.h
+++ lib/loop.h
@@ -14,6 +14,7 @@ class Loop {
   float GetRate() const { return rate; }
   float Process(float &in, float *buf);
   enum State { STOPPED, PLAYING, FADING_IN, FADING_OUT };
+  enum StateRecording { REC_STOPPED, REC_MOVING, REC_FADE_IN, REC_FADE_OUT };
 
  private:
   void ResetHead(uint8_t head);
@@ -25,11 +26,20 @@ class Loop {
   bool head = 0;
   bool recording = false;
   State state[2] = {STOPPED, STOPPED};
-  // crossfader
+  State state_recording = STOPPED;
+  // player crossfader
   CrossFade fade;
   float fade_time = 0.1f;
   float fade_inc = 0;
   float fade_pos = 0;
+  // recorder crossfader
+  CrossFade fade_recording;
+  float fade_time_recording = 0.05f;
+  float fade_inc_recording = 0;
+  float fade_pos_recording = 0;
+  // recording info
+  size_t recording_start = 0;
+  size_t recording_end = 0;
 };
 
 #endif

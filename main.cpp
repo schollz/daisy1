07a81c96b460a0d10fbcedec9514f898c49a2823
Zack <zack.scholl@gmail.com>
2024-08-13 17:11:37 -0700

    led color

--- main.cpp
+++ main.cpp
@@ -13,7 +13,11 @@ using namespace daisysp;
 
 DaisyPod hw;
 DaisySeed daisyseed;
-Loop loops[3];
+#define NUM_LOOPS 5
+Loop loops[NUM_LOOPS];
+size_t loop_index = 0;
+#define NUM_COLORS 5
+Color my_colors[5];
 
 bool first = true;  // first loop (sets length)
 bool rec = false;   // currently recording
@@ -47,7 +51,15 @@ static void AudioCallback(AudioHandle::InterleavingInputBuffer in,
 
 int main(void) {
   hw.Init();
-  /* true == wait for PC: will block until a terminal is connected */
+
+  my_colors[0].Init(Color::PresetColor::RED);
+  my_colors[1].Init(Color::PresetColor::GREEN);
+  my_colors[2].Init(Color::PresetColor::BLUE);
+  my_colors[3].Init(Color::PresetColor::PURPLE);
+  my_colors[4].Init(Color::PresetColor::GOLD);
+  hw.led1.SetColor(my_colors[0]);
+  hw.led1.Update();
+
   daisyseed.StartLog(true);
 
   daisyseed.PrintLine("Verify CRT floating point format: %.3f", 124.0f);
@@ -113,22 +125,30 @@ void UpdateButtons() {
 uint32_t lastPrintTime = 0;
 const uint32_t printInterval = 100;  // Print every 1000 ms (1 second)
 int encoderIncrement = 0;
+float knobs_last[2] = {0, 0};
+float knobs_current[2] = {0, 0};
+bool button1_pressed = false;
+bool button2_pressed = false;
 void Controls() {
   hw.ProcessAnalogControls();
   hw.ProcessDigitalControls();
 
-  drywet = hw.knob1.Process();
+  if (hw.button1.RisingEdge()) {
+    button1_pressed = true;
+    // increment loop index
+    loop_index++;
+    if (loop_index >= NUM_LOOPS) {
+      loop_index = 0;
+    }
+    // set color
+    hw.led1.SetColor(my_colors[loop_index]);
+    hw.led1.Update();
+  }
 
-  UpdateButtons();
+  if (hw.button2.RisingEdge()) {
+    // start recording
+  }
 
-  // leds
-  hw.led1.Set(0, play == true, 0);
-  hw.led2.Set(rec == true, 0, 0);
-
-  hw.UpdateLeds();
-
-  // update encoder
-  // hw.encoder.Debounce();
   int inc = hw.encoder.Increment();
   if (inc > 0) {
     encoderIncrement++;
@@ -136,11 +156,29 @@ void Controls() {
     encoderIncrement--;
   }
 
+  // make array of knob processes
+  knobs_current[0] = hw.knob1.Process();
+  knobs_current[1] = hw.knob2.Process();
+  // round to 3 decimal places
+  knobs_current[0] = roundf(knobs_current[0] * 200) / 200;
+  knobs_current[1] = roundf(knobs_current[1] * 200) / 200;
+
   // get current time
   uint32_t currentTime = System::GetNow();
   if (currentTime - lastPrintTime >= printInterval) {
-    daisyseed.PrintLine("knob1=%2.3f knob2=%2.3f, enc=%d", hw.knob1.Process(),
-                        hw.knob2.Process(), encoderIncrement);
+    bool knob_changed = false;
+    for (size_t i = 0; i < 2; i++) {
+      if (knobs_current[i] != knobs_last[i]) {
+        knobs_last[i] = knobs_current[i];
+        knob_changed = true;
+      }
+    }
+    if (knob_changed || button1_pressed) {
+      daisyseed.PrintLine("%d, but1=%d, knob1=%2.3f knob2=%2.3f, enc=%d",
+                          loop_index, button1_pressed, knobs_current[0],
+                          knobs_current[1], encoderIncrement);
+      button1_pressed = false;
+    }
     lastPrintTime = currentTime;
   }
 }
